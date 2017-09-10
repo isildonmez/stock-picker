86eb6e1f080b890496001127f4ea9bb3e9935692
Isil Donmez <isil.donmez@gmail.com>
2017-09-10 14:41:03 +0100

    Finish the method with an alternative solution

---
+++ stock-picker.rb
@@ -0,0 +1,48 @@
+# O(n^2) solution
+def stock_picker(array)
+	profit = 0
+	days_index = []
+	array.each_with_index do |n, index|
+		m = index
+		while m < array.length
+			result = array[m] - n
+			if result >= profit
+				profit = result
+				days_index = [index, m]
+			end
+			m += 1
+		end
+	end
+	days_index
+end
+
+# O(n) solution
+def stock_picker2(my_array)
+	j = 0
+	max = my_array[1] - my_array[0]
+	table = [0, 1]
+	for i in (1...(my_array).length) do
+		if my_array[i] <= my_array[j]
+			j = i
+		end
+		if my_array[i] - my_array[j] > max
+			max =  my_array[i] - my_array[j]
+			table = [].push(j,i)
+		end
+
+	end
+	return table
+end
+
+
+puts stock_picker([17,3,6,9,15,8,6,1,10]).to_s
+puts stock_picker([17,16,16]).to_s
+puts stock_picker([17]).to_s
+puts stock_picker([16,17,17]).to_s
+puts stock_picker([17,16,17,18]).to_s
+
+puts stock_picker2([17,3,6,9,15,8,6,1,10]).to_s
+puts stock_picker2([17,16,16]).to_s
+puts stock_picker2([17]).to_s
+puts stock_picker2([16,17,17]).to_s
+puts stock_picker2([17,16,17,18]).to_s
